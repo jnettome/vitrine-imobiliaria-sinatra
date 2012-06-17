ef0376a525397a044be6d43e47e6a67a77589691
João Netto <hi@joaonetto.me>
2012-06-17 00:50:28 -0300

    Exemplo finalizado

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'erb'
@@ -5,7 +6,24 @@ require File.dirname(__FILE__) + '/models/estate.rb'
 
 set :database, 'mysql://app5315771:fNRKdpEP@instance18199.db.xeround.com.:12020/app5315771'
 
+before do
+  content_type :html, 'charset' => 'utf-8'
+end
+
 get '/' do
   @estates = Estate.all()
   erb :index
+end
+
+get '/add' do
+  erb :add
+end
+
+post '/add' do
+  @estate = Estate.new(params)
+  if @estate.save
+    redirect '/'
+  else
+    'Ocorreu algum erro ao adicionar esse imóvel...'
+  end
 end
